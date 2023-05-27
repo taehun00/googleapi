aaaf4d3c3975a3a80f66c67829e884342a20555a
admin <iiss07007@gmail.com>
2023-05-27 18:13:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/googleapi/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.googleapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val seoul = LatLng(37.56, 126.97)
+
+        val markerOptions = MarkerOptions()
+            .position(seoul)
+            .title("서울")
+            .snippet("한국의 수도")
+        mMap.addMarker(markerOptions)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
+    }
+}
